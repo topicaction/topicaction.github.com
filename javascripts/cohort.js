c5f296925aefd4b1dbbfd797f9fa83480b3042e1
Ross Kaffenberger <rosskaff@gmail.com>
2011-12-09 12:47:18 -0500

    Fixes for selecting new cohort when cohorts have changed

--- javascripts/cohort.js
+++ javascripts/cohort.js
@@ -34,6 +34,23 @@
     ];
   }
 
+  TA.detectCohort = function(cohortName, cohorts) {
+    if (cohortName == null) return null;
+    var cohort;
+
+    $.each(cohorts, function(i, coh) {
+      if (cohortName == coh.displayName) {
+        cohort = coh;
+      }
+    });
+
+    return cohort;
+  };
+
+  TA.randomlySelectNewCohort = function(cohorts) {
+    var cohortIndex = Math.floor(Math.random() * cohorts.length);
+    return cohorts[cohortIndex];
+  };
 
   TA.getCohortFromStorage = function(opts) {
     opts = opts || {};
@@ -41,23 +58,11 @@
     var cohortKey   = 'topicaction-12082011';
     var cohortName  = amplify.store(cohortKey);
     var cohorts     = TA.CohortData;
-    var cohort;
+    var cohort      = TA.detectCohort(cohortName, cohorts);
 
-    if (!cohortName || TA.Env.refresh()) {
-      // set the cohort via A/B
-      var cohortIndex = Math.floor(Math.random() * cohorts.length);
-
-      cohort      = cohorts[cohortIndex];
-      cohortName  = cohort.displayName;
-
-      amplify.store(cohortKey, cohortName);
-    } else {
-      // search for cohort
-      $.each(cohorts, function(i, coh) {
-        if (cohortName == coh.displayName) {
-          cohort = coh;
-        }
-      });
+    if (!cohort || TA.Env.refresh()) {
+      cohort = TA.randomlySelectNewCohort(cohorts);
+      amplify.store(cohortKey, cohort.displayName);
     }
 
     return cohort;
